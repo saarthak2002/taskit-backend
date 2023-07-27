b6d5aec67f59d16513f7fa5ebbbc5e3581bbbba3
Saarthak Gupta <79454214+saarthak2002@users.noreply.github.com>
2023-07-27 21:21:28 +0530

    set up for Heroku deployment

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
-from config import Config
+import os
 
 app = Flask(__name__)
 CORS(app)
-app.config.from_object(Config)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
